91cf7c7deab4e66fec44f1bd7b0194458254e200
Mike Warot <chezmike@gmail.com>
2017-05-20 18:28:06 -0500

    Change the Window title

--- Bitgrid_VB/Form1.Designer.vb
+++ Bitgrid_VB/Form1.Designer.vb
@@ -28,9 +28,9 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(559, 278)
+        Me.ClientSize = New System.Drawing.Size(689, 285)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Bitgrid Simulator"
         Me.ResumeLayout(False)
 
     End Sub
