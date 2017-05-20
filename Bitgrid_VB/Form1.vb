02df372dbd9102f6f0bc86988347b306c1cfb3b8
Mike Warot <chezmike@gmail.com>
2017-05-20 18:34:38 -0500

    Add a menu with exit and help

--- Bitgrid_VB/Form1.vb
+++ Bitgrid_VB/Form1.vb
@@ -1,3 +1,10 @@
 ﻿Public Class Form1
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Application.Exit()
 
+    End Sub
+
+    Private Sub AboutBitgridSimulatorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutBitgridSimulatorToolStripMenuItem.Click
+        MsgBox("Bitgrid Simulator - Version 0.0001", MsgBoxStyle.OkCancel)
+    End Sub
 End Class
